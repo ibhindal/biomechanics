8b398be14f2c49276977b9076e8c579538d7b9ba
ibhindal <hindali@cardiff.ac.uk>
2022-12-17 11:29:24 +0000

    update

--- biiomechanics object tracking.py
+++ biiomechanics object tracking.py
@@ -4,10 +4,10 @@ import os
 import pandas as pd
 import scipy
 import math
-os.chdir("C:\\Users\\Ibrahim\\desktop")
+#os.chdir("C:\\Users\\Ibrahim\\desktop")
 
 tracker = cv2.TrackerCSRT_create()
-video = cv2.VideoCapture('CL_1_S0001.mp4')
+video = cv2.VideoCapture('CL_1_S0003.mp4')
 ok,frame=video.read()
 bbox = cv2.selectROI(frame)
 ok = tracker.init(frame,bbox)
