d1485a07cc631700367b5d382f67e1f4e9b1c84d
ibhindal <hindali@cardiff.ac.uk>
2023-02-03 12:48:50 +0000

    yolo 1d

--- Drawingpoints.py
+++ Drawingpoints.py
@@ -1,10 +1,8 @@
 import cv2
 
-import cv2
-
 def click_event(event, x, y, flags, params):
    if event == cv2.EVENT_LBUTTONDOWN:
-    print(f'Original Coordinates : ({x},{y})')
+    #print(f'Original Coordinates : ({x},{y})')
 
     xy = x/frame.shape[1]
     yy = y/frame.shape[0]
@@ -12,6 +10,7 @@ def click_event(event, x, y, flags, params):
     cv2.putText(frame, f'({x},{y})',(x,y),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     
     cv2.circle(frame, (x,y), 8, (255,0,0), -1)
+    return xy , yy
     
 
 videopath = "CL_1_S0003.mp4"
