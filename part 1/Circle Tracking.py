e8136289f014b7d517c412d29abb56312ca764c9
ibhindal <hindali@cardiff.ac.uk>
2023-02-26 11:20:58 +0000

    reorganise

---
+++ part 1/Circle Tracking.py
@@ -0,0 +1,161 @@
+# Hough circle transform technique
+#https://www.youtube.com/watch?v=RaCwLrKuS1w
+
+
+import cv2 as cv
+import numpy as np
+import os #provides functions for interacting with operating system (OS)
+import math
+import scipy
+#os.system('cls')
+import pandas as pd
+#import telegram_send
+
+# os.chdir : changes the current working directory to specified path
+# Syntax: os.chdir(path)
+os.chdir("C:\\Users\\Ibrahim\\desktop")
+
+file_name=['CL_1_S0001.mp4']
+'''
+for x in range(1, 3, 1 ):
+ for y in range(30, 60, 10):
+    for z in range(1, 4, 1 ):
+        name =("P2_" + str(x) +"_" +str(y) +"_" + str(z)+ ".mp4")
+        #print (name)
+        file_name.append(name) 
+'''
+#print (file_name)
+
+file_name_len=len(file_name)
+for i in range(file_name_len): 
+    name = file_name[i]
+   
+    #name = 'P2_2_30_1.mp4' # white ball : P2_1_30_1.mp4 | orange ball: P2_2_30_1.mp4
+    video = cv.VideoCapture(name)  
+    prevCircle = None
+    dist = lambda x1, y1, x2, y2: (x1-x2)**2 + (y1-y2)**2
+
+    #video = cv.VideoCapture(path)
+    ball_size = 0.22 #diameter of a regulation ball in meters
+    fps_cam = 1000 # Change this to the required fps of the video 
+    fps_vid =video.get(cv.CAP_PROP_FPS)
+    fps_time= fps_vid / fps_cam 
+    #print(fps_time)
+
+    scale  = []
+    x_list = []
+    y_list = []
+
+    while True:    
+
+        #read (returns) frame from video capture
+        ret, frame = video.read()
+
+        # if frame not returned, subsequent break function is performed
+        if not ret: break
+
+    #converts frame to grayscale and blurs it. (Helps mitigate background noise)
+
+        grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        #17,17) input decides degree of blur. Higher integers increase blur
+        blurFrame = cv.GaussianBlur(grayFrame, (17,17), 0)
+        
+        #Hough Circle Transform :
+
+        circles = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1.2, 100, 
+                    param1 = 100, param2 = 30, minRadius = 100, maxRadius= 350)  # orange ball
+
+    # goes through detected circles and selects the "best" one
+
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            chosen = None
+            for i in circles[0, :]:
+                if chosen is None: chosen = i
+                if prevCircle is not None:
+                    if dist(chosen[0], chosen[1], prevCircle[0], prevCircle[1]) <= dist(i[0], i[1],prevCircle[0], prevCircle[1]):
+                        chosen = i
+        
+    #drawing circle to detect centre point of object and around the ball itself
+    # Where 3 represents thickness of circle
+            cv.circle(frame, (chosen[0], chosen[1]), 1, (0,100,100), 3)
+
+    #circle around ball or circumference of detected circle
+
+            cv.circle(frame, (chosen[0], chosen[1]), chosen[2], (255,0,255), 3)
+            prevCircle = chosen
+            #print(chosen) #these print statements are for debugging andhelp with understanding the code.
+            #they do fill up the terminal with information tho
+
+            ball_d= chosen[2]*2 #changes radius to diameter of ball
+            #print(ball_d)
+            scale.append(ball_d/ball_size)  #diameter in pixels or coordinate value / real diameter in cm to give pixel per cm for a scale factor  
+            x_list.append(chosen[0]) #list of x positions of center of ball
+            y_list.append(chosen[1]) # list of y positions of center of ball
+            
+            #print(scale)
+            
+
+        cv.imshow("circles", frame)
+        #print(float(prevCircle[dist]) - float(chosen[dist]))
+
+
+        if cv.waitKey(1) & 0xFF == ord('q'): break
+
+
+    video.release()
+    cv.destroyAllWindows()
+
+    scale_ave=scipy.stats.trim_mean(scale, 0.1) #trim_mean 10% either way to remove some extrainious results
+    
+    x_diff=[]
+    x_len=len(x_list)-1 #minus 1 as python starts with 0 so we dont overflow
+
+    for i in range(x_len): 
+            x_diff.append(x_list[i]-x_list[i+1]) #find x distance per frame
+    #print(x_diff)
+
+    y_diff=[]
+
+    for i in range(x_len): 
+        y_diff.append(y_list[i]-y_list[i+1])  #find y distance per frame
+    #print(y_diff)
+
+    pyth_dist=[]
+    pyth_sub=[]
+    x2_len=len(x_diff)-1
+
+    for i in range(x2_len):
+        pyth_sub=math.hypot(x_diff[i] , y_diff[i])
+        pyth_dist.append(pyth_sub) #do pythagoras to find pixel distance per frame
+        #print(pyth_dist)
+
+
+
+    realdist=[]
+    speed=[]
+    for i in range(x2_len):
+        realdistcalc=(pyth_dist[i]/scale_ave)
+        realdist.append(realdistcalc) # chande from pixels to meters
+
+    #print(realdist)
+
+    for item in realdist:
+        if item > 1:
+            realdist.remove(item)
+
+    distlen=len(realdist)-1
+    
+    for i in range(distlen):
+        speedcalc=realdist[i]/fps_time
+        speed.append(speedcalc)
+
+
+   # print(speed)
+    df = pd.DataFrame(speed)
+    df.to_csv('speed.csv', index=False)
+
+
+
+#telegram_send.send(messages=["The code has finished running"]) 
---
+++ part 1/biiomechanics object tracking.py
@@ -0,0 +1,248 @@
+import cv2
+import numpy as np
+import os
+import pandas as pd
+import scipy
+import math
+
+
+
+#os.chdir("C:\\Users\\Ibrahim\\desktop")
+# Set the directory to the current directory
+directory = '.'
+
+# Use os.listdir to get a list of all files in the directory
+files = os.listdir(directory)
+
+# Use a for loop to iterate through the list of files
+for file in files:
+  # Check if the file is a directory
+  if os.path.isdir(file):
+    # Skip directories
+    continue
+  # Check if the file is a video file (using the file extension)
+  elif file.endswith('.mp4'):
+    
+        tracker = cv2.TrackerCSRT_create()
+        video = cv2.VideoCapture(file)
+        ok,frame=video.read()
+        bbox = cv2.selectROI(frame)
+        ok = tracker.init(frame,bbox)
+
+        wall_bbox = cv2.selectROI(frame)
+        (x_wall,y_wall,x2_wall,y2_wall) = wall_bbox
+        print(wall_bbox)
+        num_cont_frames=0
+
+        #video = cv.VideoCapture(path)
+        ball_size = 0.22 #diameter of a regulation ball in meters
+        fps_cam = 10000 # Change this to the required fps of the video 
+        fps_vid =video.get(cv2.CAP_PROP_FPS)
+        fps_time= fps_vid / fps_cam 
+        #print(fps_time)
+
+
+        # Initialize variable to track state of ball (in contact with wall or not)
+        in_contact = False
+
+        # Initialize variable to track whether in_contact has ever been True
+        in_contact_ever = False
+
+
+        # Initialize lists for inbound and outbound velocities
+        inbound_velocities = []
+        outbound_velocities = []
+
+        # Calculate time interval between frames in seconds
+        time_interval = 1 / fps_cam
+
+        scale  = []
+        x_list = []
+        y_list = []
+        x_def=[]
+        inbound_x = []
+        inbound_y = []
+        outbound_x = []
+        outbound_y = []
+
+        while True:
+            ok,frame=video.read()
+            if not ok:
+                break
+            ok,bbox=tracker.update(frame)
+            if ok:
+
+                
+                (x,y,w,h)=[int(v) for v in bbox]
+                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2,1)
+                x2=x+w
+                y2=y+h
+                scale.append(ball_size/h)  #meters per pixel.diameter in pixels or coordinate value / real diameter in m to give pixel per m for a scale factor  
+                x_list.append(x2) #list of x positions of right edge
+                y_list.append(y2) 
+            
+                if x < max(x2_wall, x_wall): #sometimes the bbox is the wrong way around
+                    # Set in_contact to True
+                    in_contact = True
+                    # Set in_contact_ever to True
+                    in_contact_ever = True
+                    # Increment counter
+                    num_cont_frames = num_cont_frames + 1
+                    x_defe = x2-x2_wall
+                    x_def.append(x_defe) 
+                else: 
+                    in_contact = False 
+
+                if in_contact == False and in_contact_ever==False:
+                    inbound_x.append(x2) #list of x positions of right edge
+                    inbound_y.append(y2)  
+                
+                if in_contact == False and in_contact_ever==True:
+                    outbound_x.append(x2) #list of x positions of right edge
+                    outbound_y.append(y2)
+            
+            else:
+                cv2.putText(frame,'Error',(100,0),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+            cv2.imshow('Tracking',frame)
+            if cv2.waitKey(1) & 0XFF==27:
+                break
+            
+        cv2.destroyAllWindows()
+
+        scale_ave=scipy.stats.trim_mean(scale, 0.2) #trim_mean 20% either way to remove some extrainious results
+
+        x_diff=[]
+        y_diff=[]
+        x_len=len(x_list)-1 #minus 1 as python starts with 0 so we dont overflow
+
+        for i in range(x_len): 
+                x_diff.append(x_list[i]-x_list[i+1]) #find x distance per frame
+
+
+        for i in range(x_len): 
+            y_diff.append(y_list[i]-y_list[i+1])  #find y distance per frame
+
+
+        pyth_dist=[]
+        pyth_sub=[]
+        x2_len=len(x_diff)-1
+
+        for i in range(x2_len):
+            pyth_sub=math.hypot(x_diff[i] , y_diff[i])
+            pyth_dist.append(pyth_sub) #do pythagoras to find pixel distance per frame
+        
+        realdist=[]
+        speed=[]
+        for i in range(x2_len):
+            realdistcalc=(pyth_dist[i]*scale_ave)
+            realdist.append(realdistcalc) # change from pixels to meters
+
+        for item in realdist:
+            if item > 1:
+                realdist.remove(item)
+
+        distlen=len(realdist)-1
+        x_speed=[]
+        y_speed=[]
+        for i in range(x2_len):
+            x_speed=x_diff[i]*scale_ave*fps_cam
+            y_speed=y_diff[i]*scale_ave*fps_cam
+
+
+        for i in range(distlen):
+            speedcalc=realdist[i]*fps_cam
+            speed.append(speedcalc)
+
+        contact_time=num_cont_frames/fps_cam
+        print(contact_time)
+
+        if x_def:
+            realxdef = min(x_def)*scale_ave
+        else:
+            realxdef = 0
+
+        print(realxdef)
+
+        # Calculate inbound velocities
+        inbound_x_diff = []
+        inbound_y_diff = []
+        # Calculate inbound x- and y-velocities
+        inbound_x_velocities = []
+        inbound_y_velocities = []
+        inbound_len = len(inbound_x) - 1
+
+        # Calculate differences between consecutive x and y coordinates
+        for i in range(inbound_len):
+            inbound_x_diff.append(inbound_x[i] - inbound_x[i + 1])
+            inbound_y_diff.append(inbound_y[i] - inbound_y[i + 1])
+
+        # Calculate inbound velocities in meters per second
+        inbound_velocities = []
+        for i in range(inbound_len):
+            inbound_x_velocity = inbound_x_diff[i] * scale_ave * fps_cam
+            inbound_x_velocities.append(inbound_x_velocity)
+            inbound_y_velocity = inbound_y_diff[i] * scale_ave * fps_cam
+            inbound_y_velocities.append(inbound_y_velocity)
+            inbound_velocity = math.hypot(inbound_x_diff[i], inbound_y_diff[i]) * scale_ave * fps_cam
+            inbound_velocities.append(inbound_velocity)
+
+        # Calculate outbound velocities
+        outbound_x_diff = []
+        outbound_y_diff = []
+
+        outbound_len = len(outbound_x) - 1
+
+        # Calculate differences between consecutive x and y coordinates
+        for i in range(outbound_len):
+            outbound_x_diff.append(outbound_x[i] - outbound_x[i + 1])
+            outbound_y_diff.append(outbound_y[i] - outbound_y[i + 1])
+
+        # Calculate outbound velocities in meters per second
+        outbound_velocities = []
+        outbound_x_velocities = []
+        outbound_y_velocities = []
+
+        for i in range(outbound_len):
+            outbound_x_velocity = outbound_x_diff[i] * scale_ave * fps_cam
+            outbound_x_velocities.append(outbound_x_velocity)
+            outbound_y_velocity = outbound_y_diff[i] * scale_ave * fps_cam
+            outbound_y_velocities.append(outbound_y_velocity)
+            outbound_velocity = math.hypot(outbound_x_diff[i], outbound_y_diff[i]) * scale_ave * fps_cam
+            outbound_velocities.append(outbound_velocity)
+
+        corrected_average_inbound_x_velocities = scipy.stats.trim_mean(inbound_x_velocities, 0.2)
+        corrected_average_inbound_y_velocities = scipy.stats.trim_mean(inbound_y_velocities, 0.2)
+        corrected_average_inbound_velocities = scipy.stats.trim_mean(inbound_velocities, 0.2)
+        corrected_average_outbound_x_velocities = scipy.stats.trim_mean(outbound_x_velocities, 0.2)
+        corrected_average_outbound_y_velocities = scipy.stats.trim_mean(outbound_y_velocities, 0.2)
+        corrected_average_outbound_velocities = scipy.stats.trim_mean(outbound_velocities, 0.2)
+
+
+        # Create a dictionary with the data for the table
+        speeddata={'x_speed': x_speed, 'y_speed': y_speed, 'speed': speed, 'inbound_x_velocities' : inbound_x_velocities, 'inbound_y_velocities' : inbound_y_velocities, 'inbound_velocities' : inbound_velocities , 'outbound_x_velocities' : outbound_x_velocities, 'outbound_y_velocities' : outbound_y_velocities, 'outbound_velocities' : outbound_velocities, 'corrected_average_inbound_x_velocities ': corrected_average_inbound_x_velocities, 'corrected_average_inbound_y_velocities': corrected_average_inbound_y_velocities, 'corrected_average_inbound_velocities': corrected_average_inbound_velocities, 'corrected_average_outbound_x_velocities': corrected_average_outbound_x_velocities, 'corrected_average_outbound_y_velocities': corrected_average_outbound_y_velocities, 'corrected_average_outbound_velocities': corrected_average_outbound_velocities, 'contact_time': contact_time, 'deformation' :realxdef}
+
+        # Find the length of the longest array
+        max_length = max(len(a) for a in speeddata.values() if isinstance(a, list))
+
+
+
+        # Iterate over the items in speeddata
+        for key, value in speeddata.items():
+            # If the value is a single float, pad it with np.nan values to make it a list
+            if not isinstance(value, list):
+                value = [value] * max_length
+            # If the value is an array, pad it with np.nan values if it is shorter than the longest array
+            else:
+                value = value + [np.nan] * (max_length - len(value))
+            speeddata[key] = value
+
+        # Create a new DataFrame using the padded arrays
+        df = pd.DataFrame(speeddata)
+        # Export the DataFrame to a CSV file
+        filename = file + 'results.csv'
+        df.to_csv(filename, index=False)
+
+  else:
+    # Skip non-video files
+    continue
+
